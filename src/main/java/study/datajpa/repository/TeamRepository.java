29737f361952617b2d9b7c4726fa90541dab894e
ymkim <ymkim1085@funin.camp>
2022-06-08 00:03:39 +0900

    feat: 순수 JPA 기반 리포지토리 생성
    스프링 데이터 JPA를 사용하지 않고 순수 JPA를 기반으로 하여 Member, Team 리포지토리 생성 후 CRUD 테스트 코드 작성

---
+++ src/main/java/study/datajpa/repository/TeamRepository.java
@@ -0,0 +1,43 @@
+package study.datajpa.repository;
+
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamRepository {
+
+    @PersistenceContext
+    private EntityManager em; // JPA EntityManger
+
+    // 저장
+    public Team saveTeam(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    // 삭제
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    // 전체 조회
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class).getResultList();
+    }
+
+    // 단건 조회
+    public Optional<Team> findById(int id) {
+        Team team = em.find(Team.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    // 카운팅
+    public long count() {
+        return em.createQuery("select t from Team t", Long.class).getSingleResult();
+    }
+}
--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -1,6 +1,7 @@
 package study.datajpa.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,20 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@DisplayName("순수 JPA 기반 테스트 진행")
 class MemberJpaRepositoryTest {
 
     @Autowired MemberJpaRepository memberJpaRepository;
 
     @Test
+    @DisplayName("맴버 저장 후 조회 테스트")
     public void testMember() throws Exception {
         //given
         Member member = new Member("memberA");
@@ -30,5 +36,42 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
         assertThat(findMember).isEqualTo(member); // 동일한 하나의 Transaction 단위에서 JPA는 동일한 객체 반환을 보장 한다, 1차 캐시
+    }
+
+    @Test
+    @Rollback(false)
+    @DisplayName("기본 CRUD 기능 테스트")
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        //when
+        Member savedMember1 = memberJpaRepository.save(member1);
+        Member savedMember2 = memberJpaRepository.save(member2);
+        Member findMember1 = memberJpaRepository.findById(savedMember1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(savedMember2.getId()).get();
+
+        //then
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //HINT : 하나의 Transaction 단위 내에서 엔티티 수정 시 dirty checking(변경 감지)가 발생한다
+        findMember1.setUserName("member!!!!!!!!");
+
+        //리스트 조회 검증
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2); // 위에서 등록한 Member는 2명, size = 2 (success)
+
+        //카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
