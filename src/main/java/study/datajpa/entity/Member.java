531d5de165b5825b5766d58f09921734b703a430
ymkim <ymkim1085@funin.camp>
2022-06-07 22:15:40 +0900

    feat: 예제 도메인 모델
    Member, Team 연관관계 설정 후
    Member 엔티티 테스트 코드 작성

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -2,23 +2,45 @@ package study.datajpa.entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"}) // 연관관계 필드는 @ToString 지양
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String userName;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id") // FK
+    private Team team;
+
+//    protected Member() {
+//    }
 
     public Member(String userName) {
         this.userName = userName;
+    }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    // 연관관계 편의 메서드 생성
+    // 1(Member) : N(Team)
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
