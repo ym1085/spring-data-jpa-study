2cd59dcd5f16149b2b71e3fcb4817960df3663f1
ymkim <ymkim1085@funin.camp>
2022-06-08 22:18:06 +0900

    feat: 공통 인터페이스 관련 정리
    스프링 데이터 JPA가 제공하는 공통 인터페이스 관련하여 간략히 구현해보고, 정리

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package study.datajpa.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,6 +9,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +24,8 @@ class MemberRepositoryTest {
     @Test
     @Rollback(false)
     public void test() throws Exception {
+        System.out.println("TEST = memberRepository = " + memberRepository.getClass());
+
         //given
         Member member = new Member("memberA");
 
@@ -33,5 +37,42 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    @Rollback(false)
+    @DisplayName("기본 CRUD 기능 테스트")
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        //when
+        Member savedMember1 = memberRepository.save(member1);
+        Member savedMember2 = memberRepository.save(member2);
+        Member findMember1 = memberRepository.findById(savedMember1.getId()).get();
+        Member findMember2 = memberRepository.findById(savedMember2.getId()).get();
+
+        //then
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //HINT : 하나의 Transaction 단위 내에서 엔티티 수정 시 dirty checking(변경 감지)가 발생한다
+        findMember1.setUserName("member!!!!!!!!");
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2); // 위에서 등록한 Member는 2명, size = 2 (success)
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
     }
 }
