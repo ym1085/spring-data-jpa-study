f7f778416acbf1792a4c8a6305e8ead3294e3a1d
ymkim <ymkim1085@funin.camp>
2022-06-11 15:08:17 +0900

    feat: 순수 JPA 기반 페이징, 정렬 실습

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -61,4 +61,19 @@ public class MemberJpaRepository {
                  .getResultList();
     }
 
+    // paging
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member m where m.age = :age order by m.userName desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    // total count
+    public Long totalCount(int age) {
+        return em.createQuery("select count(m) from Member m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
 }
