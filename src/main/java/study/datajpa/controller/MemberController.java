688d7df499a3b3c3ac1a66fa1a365778ccd46b7a
ymkim <ymkim1085@funin.camp>
2022-06-24 21:21:11 +0900

    feat: 도메인 클래스 컨버터
    * 파라미터로 id를 설정하고 엔티티 매핑시 스프링 데이터 JPA가 중간에서 컨버팅을 하여 데이터 반한하는 기능 적용

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,35 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @PostConstruct
+    public void init() {
+        memberRepository.save(new Member("userA"));
+    }
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUserName();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUserName();
+    }
+}
